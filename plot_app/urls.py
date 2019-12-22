0a0414c276e0f786df3230416dda091e6d7d6f76
KarstenPoddig <karsten_poddig@web.de>
2019-12-22 10:22:00 +0100

    Einbauen der ersten Grafik auf ".../plot_app/line_chart/"

--- plot_app/urls.py
+++ plot_app/urls.py
@@ -2,8 +2,14 @@ from django.urls import path
 from django.conf.urls import url
 from . import views
 
-app_name = 'plot_app'
+
 urlpatterns = [
     url(r'^$', views.ChoiceView.as_view(), name='choice'),
     url(r'^(?P<choice_id>[0-9]+)/vote/$', views.klick, name='klick'),
+
+    # Line chart
+    url(r'^line_chart/$', views.line_chart,
+        name='line_chart'),
+    url(r'^line_chart/json/$', views.line_chart_json,
+        name='line_chart_json'),
 ]
--- plot_app/views.py
+++ plot_app/views.py
@@ -5,6 +5,8 @@ from django.shortcuts import get_object_or_404
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
+from django.views.generic import TemplateView
+from chartjs.views.lines import BaseLineChartView
 
 
 # Create your views here.
@@ -24,4 +26,25 @@ def klick(request, choice_id):
     choice.number += 1
     choice.save()
 
-    return HttpResponseRedirect(reverse('plot_app:choice'))
+    return HttpResponseRedirect(reverse('choice'))
+
+
+class LineChartJSONView(BaseLineChartView):
+    def get_labels(self):
+        """Return 7 labels for the x-axis."""
+        return ["January", "February", "March", "April", "May", "June", "July"]
+
+    def get_providers(self):
+        """Return names of datasets."""
+        return ["Central", "Eastside", "Westside"]
+
+    def get_data(self):
+        """Return 3 datasets to plot."""
+
+        return [[75, 44, 92, 11, 44, 95, 35],
+                [41, 92, 18, 3, 73, 87, 92],
+                [87, 21, 94, 3, 90, 13, 65]]
+
+
+line_chart = TemplateView.as_view(template_name='line_chart.html')
+line_chart_json = LineChartJSONView.as_view()
